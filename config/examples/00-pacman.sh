5c3076268c302c3b9b477ea25f4d45b2f9b5181a
Carl Kittelberger <icedream@icedream.pw>
2023-11-23 22:58:45 +0100

    Fix up pacman repo configs.
    Fixes #15.

--- config/examples/00-pacman.sh
+++ config/examples/00-pacman.sh
@@ -19,5 +19,39 @@ run --write pacman-key --init
 run --slot=self pacman-key -e | run --write pacman-key --import
 run --write pacman-key --populate archlinux --populate holo
 
+# Fix up pacman repo configurations
+steamdeck_packages_base_url="https://steamdeck-packages.steamos.cloud/archlinux-mirror"
+patch_repo() {
+  local name
+  name="$1"
+  if ! run grep -qE '^\['"$name"'.*\]' /etc/pacman.conf; then
+    #echo "Skipping fixup of unused repository $name" >&2
+    return
+  fi
+  echo "Checking correct repository name for $name..." >&2
+  local version
+  version="${2:-auto}"
+  if [ "$version" = "auto" ]; then
+    version="rel"
+    VERSION_ID=
+    eval "$(cat /etc/os-release | grep '^VERSION_ID=')"
+    if [ "$VERSION_ID" != "" ]; then
+      for v in "${VERSION_ID}" "${VERSION_ID%.*}"; do
+        if curl --fail -sLIo /dev/null "$steamdeck_packages_base_url/$name-$v/os/$(uname -m)/$name-$v.db"; then
+          version="$v"
+          break
+        fi
+      done
+    fi
+  fi
+  echo "Fixing up repository $name to $name-$version" >&2
+  run sed -i 's/^\['"$name"'.*\]/['"$name"'-'"$version"']/' /etc/pacman.conf
+}
+for name in core community extra holo jupiter kde-unstable multilib testing; do
+  for suffix in "" -debug -staging -testing -testing-debug -testing-staging -testing-debug-staging; do
+    patch_repo "$name$suffix"
+  done
+done
+
 # Reinstall fakeroot for makepkg to work
 run --write pacman -S --noconfirm fakeroot
