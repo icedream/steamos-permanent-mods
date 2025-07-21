c852d022850f72c827cfded0bb81f64f0f4f8fcc
Carl Kittelberger <icedream@icedream.pw>
2025-07-21 14:18:52 +0200

    Remove ffmpeg-ndi example script.
    It's outdated.

--- config/examples/80-ffmpeg-ndi.sh
+++
@@ -1,7 +0,0 @@
-
-echo "This is currently non-functional, skipping." >&2
-
-# install ffmpeg-ndi (while reinstalling everything else)
-# TODO - does not work yet due to packages which are dependencies of other packages getting replaced and --noconfirm not allowing that to happen
-#run --write yay --noconfirm -R gamescope freerdp
-#run --write yay --noconfirm -S vmaf-git ffmpeg-ndi freerdp-git gamescope-git
